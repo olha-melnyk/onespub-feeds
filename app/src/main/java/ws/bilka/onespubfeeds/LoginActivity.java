48c7a4aa52c79985a6d37ea7995205c8c121ffbe
Olha Melnyk <olhamelnyk34@gmail.com>
2016-06-26 09:39:11 +0300

    Added VK authorization

--- app/src/main/java/ws/bilka/onespubfeeds/LoginActivity.java
+++ app/src/main/java/ws/bilka/onespubfeeds/LoginActivity.java
@@ -1,13 +1,162 @@
 package ws.bilka.onespubfeeds;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class LoginActivity extends AppCompatActivity {
+import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.VKCallback;
+import com.vk.sdk.VKScope;
+import com.vk.sdk.VKSdk;
+import com.vk.sdk.api.VKError;
+
+public class LoginActivity extends FragmentActivity {
+
+    private boolean isResumed = false;
+    private static final String[] sMyScope = new String[]{
+            VKScope.FRIENDS,
+            VKScope.WALL,
+            VKScope.PHOTOS,
+            VKScope.NOHTTPS,
+            VKScope.MESSAGES,
+            VKScope.DOCS
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        VKSdk.wakeUpSession(this, new VKCallback<VKSdk.LoginState>() {
+            @Override
+            public void onResult(VKSdk.LoginState res) {
+                if (isResumed) {
+                    switch (res) {
+                        case LoggedOut:
+                            showLogin();
+                            break;
+                        case LoggedIn:
+                            showLogout();
+                            break;
+                        case Pending:
+                            break;
+                        case Unknown:
+                            break;
+                    }
+                }
+            }
+
+            @Override
+            public void onError(VKError error) {
+            }
+        });
+
+
+    }
+
+    private void showLogout() {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.container, new LogoutFragment())
+                .commitAllowingStateLoss();
+    }
+
+    private void showLogin() {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.container, new LoginFragment())
+                .commitAllowingStateLoss();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        isResumed = true;
+        if (VKSdk.isLoggedIn()) {
+            showLogout();
+        } else {
+            showLogin();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        isResumed = false;
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        VKCallback<VKAccessToken> callback = new VKCallback<VKAccessToken>() {
+            @Override
+            public void onResult(VKAccessToken res) {
+                startFeedsActivity();
+            }
+
+            @Override
+            public void onError(VKError error) {
+            }
+        };
+
+        if (!VKSdk.onActivityResult(requestCode, resultCode, data, callback)) {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    private void startFeedsActivity() {
+        startActivity(new Intent(this, FeedsActivity.class));
+    }
+
+    public static class LoginFragment extends android.support.v4.app.Fragment {
+        public LoginFragment() {
+            super();
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View v = inflater.inflate(R.layout.fragment_login, container, false);
+            v.findViewById(R.id.sign_in_button).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    VKSdk.login(getActivity(), sMyScope);
+                }
+            });
+            return v;
+        }
+    }
+
+    public static class LogoutFragment extends android.support.v4.app.Fragment {
+        public LogoutFragment() {
+            super();
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View v = inflater.inflate(R.layout.fragment_logout, container, false);
+            v.findViewById(R.id.continue_button).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    ((LoginActivity) getActivity()).startFeedsActivity();
+                }
+            });
+
+            v.findViewById(R.id.logout).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    VKSdk.logout();
+                    if (!VKSdk.isLoggedIn()) {
+                        ((LoginActivity) getActivity()).showLogin();
+                    }
+                }
+            });
+            return v;
+        }
     }
 }
